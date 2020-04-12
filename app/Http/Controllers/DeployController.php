d5f2eb9b4649f9c6df7d7d7991468d4b35546cdf
Omar Taher Saad <omartahersaad@outlook.com>
2020-04-12 12:29:03 +0200

    Fixes for autodeploy

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -14,8 +14,7 @@ class DeployController extends Controller
         $localToken = config('app.deploy_secret');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
         if (hash_equals($githubHash, $localHash)) {
-            $root_path = base_path();
-            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+            $process = new Process('cd ~/thanawyahelwa.org; ./deploy.sh');
             $process->run(function ($type, $buffer) {
                 echo $buffer;
             });
